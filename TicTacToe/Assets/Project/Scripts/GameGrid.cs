e2179337a9f0f2d3bb42005a47514a89f5294992
pasha7768 <grimmyfunz@gmail.com>
2021-03-16 18:49:29 +0200

    Added / and \ combination

--- TicTacToe/Assets/Project/Scripts/GameGrid.cs
+++ TicTacToe/Assets/Project/Scripts/GameGrid.cs
@@ -52,6 +52,16 @@ public class GameGrid : MonoBehaviour
             }
         }
 
+        if (cellGrid[0, 0].GetPlayerSymbol() != PlayerSymbol.Void && cellGrid[0, 0].GetPlayerSymbol() == cellGrid[1, 1].GetPlayerSymbol() && cellGrid[1, 1].GetPlayerSymbol() == cellGrid[2, 2].GetPlayerSymbol())
+        {
+            return cellGrid[0, 0].GetPlayerSymbol();
+        }
+
+        if (cellGrid[2, 0].GetPlayerSymbol() != PlayerSymbol.Void && cellGrid[2, 0].GetPlayerSymbol() == cellGrid[1, 1].GetPlayerSymbol() && cellGrid[1, 1].GetPlayerSymbol() == cellGrid[0, 2].GetPlayerSymbol())
+        {
+            return cellGrid[2, 0].GetPlayerSymbol();
+        }
+
         int counter = 0;
 
         foreach (Cell item in cellGrid)
